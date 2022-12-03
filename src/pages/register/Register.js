44a0e948b5745413a36c09b5bfcbe9540d3aee17
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-12-03 09:12:32 +0300

    Create register page that user will use when signing up

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,7 +1,33 @@
 import "./register.scss";
 
 const Register = () => {
-  return <div></div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
