b44636b8932482c0159a9c05f5fbf323f36a771f
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-12-07 06:52:20 +0300

    Create posts component to display post the user will see from their friends

---
+++ src/components/post/Post.js
@@ -0,0 +1,49 @@
+import "./post.scss";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
+import { Link } from "react-router-dom";
+
+const Post = ({ post }) => {
+  const liked = false;
+  return (
+    <div className="post">
+      <div className="container">
+        <div className="user">
+          <div className="userInfo">
+            <img src={post.profilePic} alt="post" />
+            <div className="details">
+              <Link to={`/profile/${post.userId}`} style={{ color: "inherit" }}>
+                <span className="name">{post.name}</span>
+              </Link>
+              <span className="date"> 1 min ago</span>
+            </div>
+          </div>
+          <MoreHorizOutlinedIcon />
+        </div>
+        <div className="content">
+          <p>{post.desc}</p>
+          <img src={post.img} alt="content" />
+        </div>
+        <div className="info">
+          <div className="item">
+            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+            12 Likes
+          </div>
+          <div className="item">
+            <TextsmsOutlinedIcon />
+            12 Comments
+          </div>
+          <div className="item">
+            <ShareOutlinedIcon />
+            Share
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,7 +1,33 @@
+import Post from "../post/Post";
 import "./posts.scss";
 
 const Posts = () => {
-  return <div className="posts">Posts</div>;
+  const posts = [
+    {
+      id: 1,
+      name: "Jane Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      desc: " Lorem Ipsum is simply dummy text of the printing and typesetting    industry. Lorem Ipsum has been the industry's standard dummy text    ever since the 1500s, when an unknown printer took a galley of type    and scrambled it to make a type specimen book.",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      desc: " Lorem Ipsum is simply dummy text of the printing and typesetting    industry. Lorem Ipsum has been the industry's standard dummy text    ever since the 1500s, when an unknown printer took a galley of type    and scrambled it to make a type specimen book.",
+    },
+  ];
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
