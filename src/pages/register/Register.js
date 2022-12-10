60e5fd9ab3897139a0e3c714594ce33cd3f23a7a
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-12-10 14:49:13 +0300

    Bind the register page credentials with the server

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,7 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./register.scss";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState(null);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3030/api/auth/register",
+        inputs
+      );
+      data && window.location.replace("/login");
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
+
   return (
     <div className="register">
       <div className="card">
@@ -21,11 +51,32 @@ const Register = () => {
         </div>
         <div className="right">
           <h1>Register</h1>
-          <form>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            {error && error}
             <button>Register</button>
           </form>
         </div>
